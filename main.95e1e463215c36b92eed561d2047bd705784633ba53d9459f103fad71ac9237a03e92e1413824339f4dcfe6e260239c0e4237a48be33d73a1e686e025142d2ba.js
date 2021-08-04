52b02c8cb54501599534a5d9e5bd6579831705bb
Dav1com <Dav1com@users.noreply.github.com>
2021-08-04 03:30:03 +0000

    deploy: eb090299bc351cf4f07ac43008046130d899840f

--- main.95e1e463215c36b92eed561d2047bd705784633ba53d9459f103fad71ac9237a03e92e1413824339f4dcfe6e260239c0e4237a48be33d73a1e686e025142d2ba.js
+++ main.95e1e463215c36b92eed561d2047bd705784633ba53d9459f103fad71ac9237a03e92e1413824339f4dcfe6e260239c0e4237a48be33d73a1e686e025142d2ba.js
@@ -127,6 +127,5 @@ prefetcher.href=url
 document.head.appendChild(prefetcher)
 prefetches.add(url)}
 ;
-if(localStorage.getItem('theme')==='dark'){document.body.classList.add('dark');document.querySelectorAll('.table').forEach(element=>{element.classList.add('table-dark');});}
-document.getElementById('mode').addEventListener('click',()=>{document.body.classList.toggle('dark');document.querySelectorAll('.table').forEach(element=>{element.classList.toggle('table-dark');})
-localStorage.setItem('theme',document.body.classList.contains('dark')?'dark':'light');});
+if(localStorage.getItem('theme')==='dark'){document.body.classList.add('dark');}
+document.getElementById('mode').addEventListener('click',()=>{document.body.classList.toggle('dark');localStorage.setItem('theme',document.body.classList.contains('dark')?'dark':'light');});
