bd308ecb58cbeb7aa131a0be7070ced90becce78
Dav1com <42521812+Dav1com@users.noreply.github.com>
2021-08-03 23:28:12 -0400

    Now tables dark mode is in pure CSS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,13 @@
 if (localStorage.getItem('theme') === 'dark') {
 
   document.body.classList.add('dark');
-  document.querySelectorAll('.table').forEach(element => {
-    element.classList.add('table-dark');
-  });
 
 }
 
 document.getElementById('mode').addEventListener('click', () => {
 
   document.body.classList.toggle('dark');
-  document.querySelectorAll('.table').forEach(element => {
-    element.classList.toggle('table-dark');
-  })
 
   localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
 
 });
-
