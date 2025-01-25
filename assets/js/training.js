b02f20969bd926f51e50af08e58f068003d51caa
JavOliva <jav.oliva.silva@gmail.com>
2025-01-25 16:09:01 -0300

    fetch full standings

--- assets/js/training.js
+++ assets/js/training.js
@@ -6,199 +6,7 @@ const trainingNames = [
     "Programación Dinámica",
     "Estructuras de Datos"
 ]
-const trainingProblems = [
-    [
-        {
-            problemName: "Books",
-            problemId: "A",
-            problemLetter: "A",
-            problemUrl: "https://codeforces.com/gym/567665/problem/A",
-            categoryIndex: 0,
-            idx: 0
-        },
-        {
-            problemName: "Hamburgers",
-            problemId: "B", 
-            problemLetter: "B",
-            problemUrl: "https://codeforces.com/gym/567665/problem/B",
-            categoryIndex: 0,
-            idx: 1
-        },
-        {
-            problemName: "Sagheer and Nubian Market",
-            problemId: "C",
-            problemLetter: "C",
-            problemUrl: "https://codeforces.com/gym/567665/problem/C",
-            categoryIndex: 0,
-            idx: 2
-        },
-        {
-            problemName: "Bonuses on a Line",
-            problemId: "D",
-            problemLetter: "D",
-            problemUrl: "https://codeforces.com/gym/567665/problem/D",
-            categoryIndex: 0,
-            idx: 3
-        },
-        {
-            problemName: "Final Boss",
-            problemId: "E",
-            problemLetter: "E",
-            problemUrl: "https://codeforces.com/gym/567665/problem/E",
-            categoryIndex: 0,
-            idx: 4
-        },
-        {
-            problemName: "Jumping Through Segments",
-            problemId: "F",
-            problemLetter: "F",
-            problemUrl: "https://codeforces.com/gym/567665/problem/F",
-            categoryIndex: 0,
-            idx: 5
-        },
-        {
-            problemName: "Queries about less than or equal elements",
-            problemId: "G",
-            problemLetter: "G",
-            problemUrl: "https://codeforces.com/gym/567665/problem/G",
-            categoryIndex: 0,
-            idx: 6
-        }
-    ],
-    [
-        {
-            problemName: "Two Buttons",
-            problemId: "A",
-            problemLetter: "A",
-            problemUrl: "https://codeforces.com/gym/567946/problem/A",
-            categoryIndex: 1,
-            idx: 0
-        },
-        {
-            problemName: "Counting Rooms",
-            problemId: "B",
-            problemLetter: "B",
-            problemUrl: "https://codeforces.com/gym/567946/problem/B",
-            categoryIndex: 1,
-            idx: 1
-        },
-        {
-            problemName: "Labyrinth",
-            problemId: "C",
-            problemLetter: "C",
-            problemUrl: "https://codeforces.com/gym/567946/problem/C",
-            categoryIndex: 1,
-            idx: 2
-        },
-        {
-            problemName: "Dijkstra?",
-            problemId: "D",
-            problemLetter: "D",
-            problemUrl: "https://codeforces.com/gym/567946/problem/D",
-            categoryIndex: 1,
-            idx: 3
-        },
-        {
-            problemName: "Even Walk",
-            problemId: "E",
-            problemLetter: "E",
-            problemUrl: "https://codeforces.com/gym/567946/problem/E",
-            categoryIndex: 1,
-            idx: 4
-        },
-        {
-            problemName: "Pizzas",
-            problemId: "F",
-            problemLetter: "F",
-            problemUrl: "https://codeforces.com/gym/567946/problem/F",
-            categoryIndex: 1,
-            idx: 5
-        },
-        {
-            problemName: "Treasures",
-            problemId: "G",
-            problemLetter: "G",
-            problemUrl: "https://codeforces.com/gym/567946/problem/G",
-            categoryIndex: 1,
-            idx: 5
-        }
-    ],
-    [
-        {
-            problemName: "CutRibbon",
-            problemId: "A",
-            problemLetter: "A",
-            problemUrl: "https://codeforces.com/gym/567947/problem/A",
-            categoryIndex: 2,
-            idx: 0
-        },
-        {
-            problemName: "Dice Combinations",
-            problemId: "B",
-            problemLetter: "B",
-            problemUrl: "https://codeforces.com/gym/567947/problem/B",
-            categoryIndex: 2,
-            idx: 1
-        },
-        {
-            problemName: "Investigating Zeroes and Ones",
-            problemId: "C",
-            problemLetter: "C",
-            problemUrl: "https://codeforces.com/gym/567947/problem/C",
-            categoryIndex: 2,
-            idx: 2
-        },
-        {
-            problemName: "2^sort",
-            problemId: "D",
-            problemLetter: "D",
-            problemUrl: "https://codeforces.com/gym/567947/problem/D",
-            categoryIndex: 2,
-            idx: 3
-        },
-        {
-            problemName: "Woodcutters",
-            problemId: "E",
-            problemLetter: "E", 
-            problemUrl: "https://codeforces.com/gym/567947/problem/E",
-            categoryIndex: 2,
-            idx: 4
-        },
-        {
-            problemName: "Flowers",
-            problemId: "F",
-            problemLetter: "F",
-            problemUrl: "https://codeforces.com/gym/567947/problem/F",
-            categoryIndex: 2,
-            idx: 5
-        },
-        {
-            problemName: "Working out",
-            problemId: "G",
-            problemLetter: "G",
-            problemUrl: "https://codeforces.com/gym/567947/problem/G",
-            categoryIndex: 2,
-            idx: 6
-        }
-    ],
-    [
-        {
-            problemName: "Static Range Sum Queries",
-            problemId: "A",
-            problemLetter: "A",
-            problemUrl: "https://codeforces.com/gym/568427/problem/A",
-            categoryIndex: 3,
-            idx: 0
-        }
-    ]
-];
 
-const contest_ids = {
-    "567665": 0,
-    "567946": 1,
-    "567947": 2,
-    "568427": 3
-};
 const contests_ids = [
     "567665",
     "567946",
@@ -258,9 +66,29 @@ async function fetchContestStatus(contestId, from, count, asManager, apiKey, sec
     return data.result;
 }
 
+async function fetchContestStandings(contestId, apiKey, secret) {
+    const asManager = false, from = 1, count = 30, showUnofficial = true;
+    const methodName = "contest.standings"
+    const params = { contestId, from, count, asManager, showUnofficial };
+    const { apiSig, time } = await generateApiSig(methodName, params, apiKey, secret);
+    const url = `https://codeforces.com/api/${methodName}?apiKey=${apiKey}&asManager=${asManager}&contestId=${contestId}&count=${count}&from=${from}&showUnofficial=${showUnofficial}&time=${time}&apiSig=${apiSig}`;
+    const response = await fetch(url);
+    if (!response.ok) {
+        console.error("Error fetching contest standings:", response.statusText);
+        return [];
+    }
+    const data = await response.json();
+    if (data.status !== "OK") {
+        console.error("API returned an error:", data.comment);
+        return [];
+    }
+    return data.result;
+}
+
+// 
 async function loadData(data){
 
-    for (let i = 0; i < trainingProblems.length; i ++){
+    for (let i = 0; i < trainingNames.length; i ++){
         const tabContent = document.getElementById(`tab-content-${i}`);
         if (!tabContent){
             console.error(`Tab content ${i} not found.`);
@@ -272,24 +100,55 @@ async function loadData(data){
             console.error(`Table not found in tab ${i}.`);
             return;
         }
-
-        correct_order_users = data[0].sort((id1, id2) => {
-            if (id2.solves[i] != id1.solves[i])
-                return id2.solves[i] - id1.solves[i];
-            if (id2.nickname < id1.nickname)
-                return 1;
-            return -1;
-        });
+        const contestId = contests_ids[i];
+        contest_data = data[contestId];
+        problem_list = contest_data[0];
 
         let totalsNames = table.getElementsByTagName('tr')[0].children;
-
-        for (let j = 0; j < correct_order_users.length; j ++){
-            //totalsNames[j + 1].innerText = correct_order_users[j].nickname;
-            totalsNames[j + 1].style.fontWeight = "bold";
-
-            let crating = correct_order_users[j].crating
-            let arating = correct_order_users[j].arating
-
+        
+        let users2 = {}
+        for (let j = 0; j < users.length; j ++){
+            users2[users[j].codeforcesId] = {
+                "crating": users[j].codeforcesRating,
+                "arating": users[j].atcoderRating
+            };
+        }
+        let usedcols = 0;
+        for (let j = 0; j < contest_data[1].length; j ++){
+            user_handle = contest_data[1][j].handle;
+            if (user_handle in users2){
+                totalsNames[usedcols + 1].style.fontWeight = "bold";
+                // user is relevant
+                let crating = users2[user_handle].crating;
+                let arating = users2[user_handle].arating;
+                let className = "";
+                if (crating < 800 && arating < 200) {
+                    className = "rating-black";
+                } else if (crating <= 1199 && arating <= 399) {
+                    className = "rating-gray";
+                } else if (crating <= 1399 && arating <= 799) {
+                    className = "rating-green";
+                } else if (crating <= 1599 && arating <= 1299) {
+                    className = "rating-cyan";
+                } else if (crating <= 1899 && arating <= 1699) {
+                    className = "rating-blue";
+                } else if (crating <= 2099 && arating <= 1999) {
+                    className = "rating-purple";
+                } else if (crating <= 2399 && arating <= 2299) {
+                    className = "rating-orange";
+                } else if (crating <= 2999 && arating <= 2999) {
+                    className = "rating-red";
+                }
+                totalsNames[usedcols + 1].innerHTML = `<span class="${className}">${user_handle}</span>`;
+                delete users2[user_handle];
+                usedcols += 1;
+            }
+        }
+        for (const [user_handle, value] of Object.entries(users2)){
+            totalsNames[usedcols + 1].style.fontWeight = "bold";
+            // user is relevant
+            let crating = value.crating;
+            let arating = value.arating;
             let className = "";
             if (crating < 800 && arating < 200) {
                 className = "rating-black";
@@ -308,8 +167,8 @@ async function loadData(data){
             } else if (crating <= 2999 && arating <= 2999) {
                 className = "rating-red";
             }
-            totalsNames[j + 1].innerHTML = `<span class="${className}">${correct_order_users[j].nickname}</span>`;
-
+            totalsNames[usedcols + 1].innerHTML = `<span class="${className}">${user_handle}</span>`;
+            usedcols += 1;
         }
 
         const row = table.insertRow();
@@ -319,13 +178,24 @@ async function loadData(data){
         totalesCell.style.backgroundColor = "#f8f9fa";
         totalesCell.textContent = "Totales";
         totalesCell.style.fontWeight = "bold";
-
-        for (let j = 0; j < correct_order_users.length; j ++){
+        for (let j = 0; j < contest_data[1].length; j ++){
             const userCell = row.insertCell(j + 1);
-            userCell.textContent = correct_order_users[j].solves[i];
+            let solves = 0;
+            for (let j2 = 0; j2 < contest_data[1][j].solves.length; j2 ++){
+                if (contest_data[1][j].solves[j2] == 2)
+                    solves += 1
+            }
+            userCell.textContent = solves;
+        }
+        let ccol = contest_data[1].length
+        for (const [user_handle, value] of Object.entries(users2)){
+            const userCell = row.insertCell(ccol + 1)
+            userCell.textContent = 0;
+            ccol += 1
         }
 
-        for (let j = 0; j < trainingProblems[i].length; j ++){
+
+        for (let j = 0; j < problem_list.length; j ++){
             const problemRow = table.insertRow();
 
             const problemCell = problemRow.insertCell(0);
@@ -335,116 +205,33 @@ async function loadData(data){
             problemCell.style.fontWeight = "bold";
 
             const problemLink = document.createElement("a");
-            problemLink.href = trainingProblems[i][j].problemUrl;
-            problemLink.textContent = trainingProblems[i][j].problemName;
+            problemLink.href = problem_list[j].url;
+            problemLink.textContent = problem_list[j].name;
             problemLink.target = "_blank";
             problemLink.rel = "noopener noreferrer";
-
             problemCell.appendChild(problemLink);
 
-
-            for (let k = 0; k < correct_order_users.length; k ++){
+            for (let k = 0; k < contest_data[1].length; k ++){
                 const userCell = problemRow.insertCell(k + 1);
-                const id = correct_order_users[k].idx;
-                userCell.className = `task-score ${data[1][i][j][id]}`;
+                let score_v = contest_data[1][k].solves[j];
+                let value = (score_v == 2 ? "accepted" : (score_v == 1 ? "attempted" : "notAttempted"));
+                userCell.className = `task-score ${value}`;
             }
-
+            let newk = contest_data[1].length;
+            for (const [user_handle, value] of Object.entries(users2)){
+                const userCell = problemRow.insertCell(newk + 1)
+                userCell.className = `task-score notAttemped`;
+                newk += 1
+            }
         }
     }
     return;
 }
 
 function setcache(){
-    data = [
-        [
-
-        ],
-        [
-
-        ]
-    ]
-    users.forEach((user, id) => {
-        let user_data = {
-            nickname: user.nickname,
-            codeforcesId: user.codeforcesId,
-            solves: new Array(trainingNames.length).fill(0),
-            idx: id,
-            crating: user.codeforcesRating,
-            arating: user.atcoderRating
-        };
-        data[0].push(user_data);
-    });
-
-    let n = data[0].length;
-    for (let i = 0; i < trainingProblems.length; i ++){
-        let problems = []
-        for (let j = 0; j < trainingProblems[i].length; j ++){
-            problems.push(new Array(n).fill("notAttempted"));
-        }
-        data[1].push(problems);
-    }
     return {
-        data: data,
+        data: [],
         expires: 0
-    }
-};
-
-let testData = [
-    [
-        {
-            nickname: "dariasc",
-            codeforcesId: "dariasc",
-            solves: [5, 0, 0],
-            idx: 0,
-            crating: 1660,
-            arating: 1300
-        },
-        {
-            nickname: "vivivi",
-            codeforcesId: "vivivi",
-            solves: [7, 0, 0],
-            idx: 1,
-            crating: 1500,
-            arating: 100
-        },
-        {
-            nickname: "m1tu",
-            codeforcesId: "m1tu",
-            solves: [2, 0, 0],
-            idx: 2,
-            crating: 1400,
-            arating: 100
-        },
-        {
-            nickname: "mapachetactico",
-            codeforcesId: "MapacheTactico",
-            solves: [3, 0, 0],
-            idx: 3,
-            crating: 1200,
-            arating: 0
-        }
-    ],
-    [
-        [
-            ["accepted", "attempted", "notAttempted", "accepted"],
-            ["notAttempted", "accepted", "notAttempted", "notAttempted"]
-        ],
-        [
-
-        ],
-        [
-
-        ]
-    ]
-]
-
-function getProblemIdxFromLetter(letters){
-    if (letters.length === 1) {
-        return letters.charCodeAt(0) - 'A'.charCodeAt(0);
-    } else {
-        const firstLetter = letters.charCodeAt(0) - 'A'.charCodeAt(0) + 1; // Convert first letter to 1-based index
-        const secondLetter = letters.charCodeAt(1) - 'A'.charCodeAt(0);     // Convert second letter to 0-based index
-        return firstLetter * 26 + secondLetter - 1;
     }
 };
 
@@ -476,31 +263,40 @@ async function populateTables(apiKey, secret) {
             // Fix to fill cache by standings instead of status
             gymcache = setcache();
         }
+        
+        gymcache.data = {}
 
         for (let j = 0; j < contests_ids.length; j ++){
             const contestId = contests_ids[j]
-            const submissions = await Promise.resolve(fetchContestStatus(contestId, 1, 100, true, apiKey, secret));
-
-            if (submissions){
-                submissions.forEach((submission, id) => {
-                    const verdict = submission.verdict;
-                    let res = (verdict === "OK" ? "accepted" : "attempted");
-                    const userId = submission.author.members[0].handle;
-                    const contestIdx = contest_ids[submission.contestId];
-                    const problemIdx = getProblemIdxFromLetter(submission.problem.index);
-                    for (let i = 0; i < gymcache.data[0].length; i ++){
-                        if (gymcache.data[0][i].codeforcesId === userId){
-                            let bef = gymcache.data[1][contestIdx][problemIdx][gymcache.data[0][i].idx];
-                            if (bef === res){
-    
-                            }
-                            else{
-                                gymcache.data[1][contestIdx][problemIdx][gymcache.data[0][i].idx] = res;
-                                gymcache.data[0][i].solves[contestIdx] = gymcache.data[0][i].solves[contestIdx] + 1;
-                            }
-                        }
-                    }
+            // const submissions = await Promise.resolve(fetchContestStatus(contestId, 1, 100, true, apiKey, secret));
+            const standings = await Promise.resolve(fetchContestStandings(contestId, apiKey, secret));
+            if (standings) {
+                standings_rows = standings.rows;
+                standings_problems = standings.problems;
+                data_rows = []
+                problem_names = []
+                standings_problems.forEach((problem_data, id) => {
+                    problem_names.push({
+                        "name": problem_data["name"],
+                        "url": "https://codeforces.com/gym/" + contestId + "/problem/" + problem_data["index"]
+                    });
                 });
+                standings_rows.forEach((srow, id) => {
+                    srow_handle = srow.party.members[0].handle;
+                    srow_results = srow.problemResults;
+                    results_data = []
+                    srow_results.forEach((presult, id2) => {
+                        results_data.push(presult.points > 0 ? 2 : (presult.attemptedRejectCount > 0 ? 1 : 0));
+                    });
+                    data_rows.push({
+                        "handle": srow_handle,
+                        "solves": results_data
+                    });
+                });
+                gymcache.data[contestId] = [
+                    problem_names,
+                    data_rows
+                ]
             }
         }
 
