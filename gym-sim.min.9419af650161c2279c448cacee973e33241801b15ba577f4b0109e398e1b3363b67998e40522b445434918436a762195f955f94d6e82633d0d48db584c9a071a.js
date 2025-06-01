de032d05b5af66965686a7cc95addcdf85cfbba5
JavOliva <JavOliva@users.noreply.github.com>
2025-06-01 13:59:19 +0000

    deploy: a6453b7081d1133fc0b6b7710ca96075c546dc85

--- gym-sim.min.9419af650161c2279c448cacee973e33241801b15ba577f4b0109e398e1b3363b67998e40522b445434918436a762195f955f94d6e82633d0d48db584c9a071a.js
+++
@@ -1,7 +0,0 @@
-const path=window.location.pathname;async function populateGymsTable(){try{const e=await fetch("https://joliva.cl/api/gymsToSim");if(!e.ok)throw new Error("Failed to fetch gym contests");const t=(await e.json()).result,n=document.getElementById("codeforces-gyms-sim-tbody");t.forEach(e=>{const t=document.createElement("tr"),s=e.stars??0,o="★".repeat(s);t.innerHTML=`
-                <td><a href="${e.url}" target="_blank">${e.name}</a></td>
-                <td style="color: gold; font-size: 18px;">${o}</td>
-                <td>${e.numberOfPda2025Teams}</td>
-                <td>${e.numberOfChilean2025Teams}</td>
-                <td>${e.season??""}</td>
-            `,n.appendChild(t)})}catch(e){console.error("Error loading gyms:",e)}}populateGymsTable()
